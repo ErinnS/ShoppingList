653e8b45d48b0784890522f01824a7a5ff7f1c55
ErinnS <esummers@appdev.yearup.org>
2023-10-04 14:05:53 -0400

    initial commit

---
+++ ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("eggs,bacon,cheese,bread,pizza,ice cream,milk,water,juice,chips");
+
+    }
+}
